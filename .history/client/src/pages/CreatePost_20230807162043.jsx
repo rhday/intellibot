07dd0aa048f5c600bb4f6f826c49acd4d2796e64
rhday <54642787+rhday@users.noreply.github.com>
2023-08-08 10:16:22 +0100

    ** Bug Fixes **

--- .history/client/src/pages/CreatePost_20230807162043.jsx
+++ .history/client/src/pages/CreatePost_20230807162043.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://intellibot-vl66.onrender.com/api/v1/intellibot', {
+        const response = await fetch('https://intellibot-vl66.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
